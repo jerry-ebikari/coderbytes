e1627c4c367b9f6a7a43629dab5ffcf99d8004f1
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-03-15 15:19:34 +0100

    BracketMatcher: Implemented bracketMatcher method

--- src/java/medium/bracketMatcher/BracketMatcher.java
+++ src/java/medium/bracketMatcher/BracketMatcher.java
@@ -2,6 +2,18 @@ package java.medium.bracketMatcher;
 
 public class BracketMatcher {
     public static int bracketMatcher(String str) {
-
+        int difference = 0;
+        for (char c: str.toCharArray()) {
+            switch (c) {
+                case '(':
+                    difference++;
+                    break;
+                case ')':
+                    difference--;
+                    break;
+            }
+            if (difference < 0) return 0;
+        }
+        return difference == 0 ? 1 : 0;
     }
 }
