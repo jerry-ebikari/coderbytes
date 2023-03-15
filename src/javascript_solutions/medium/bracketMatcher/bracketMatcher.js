ba375a6b6780f7c9778a3984b418f5661a30b92f
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-03-15 15:39:33 +0100

    BracketMatcher: Implemented bracketMatcher

---
+++ src/javascript_solutions/medium/bracketMatcher/bracketMatcher.js
@@ -0,0 +1,15 @@
+function bracketMatcher(str) {
+    let difference = 0;
+    for (let i = 0; i < str.length; i++) {
+        switch (str[i]) {
+            case "(":
+                difference++;
+                break;
+            case ")":
+                difference--;
+                break;
+        }
+        if (difference < 0) return "0";
+    }
+    return difference == 0 ? "1" : "0";
+}
